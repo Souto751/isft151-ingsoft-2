42cbbc606b3406b7bc932f4aecc2fcbc54053a70
Agustin Souto <agus.souto75@gmail.com>
2022-08-05 20:32:45 -0300

    Uploaded cpp-database

---
+++ cpp-database-handler-example/build.bat
@@ -0,0 +1,1 @@
+g++ -Wall -std=c++11 -I.\include .\lib\sqlite3.dll main.cpp -o .\build\ms_windows\dbh_example.exe
---
+++ cpp-database-handler-example/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+g++ -Wall -std=c++11 -I./include ./lib/libsqlite3.so use_example_refact.cpp -o ./build/gnu_linux/dbh_example.bin
+
+
+
